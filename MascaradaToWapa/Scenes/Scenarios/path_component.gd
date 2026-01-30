bdb3062e8528aa88053bf9fb829a561379f30b98
Javier Valseca <javivals@ucm.es>
2026-01-30 21:28:40 +0100

    a

--- MascaradaToWapa/Scenes/Scenarios/path_component.gd
+++ MascaradaToWapa/Scenes/Scenarios/path_component.gd
@@ -1,5 +1,7 @@
 extends Node2D
+class_name PathComponent
 
+@export var movement_component:Character
 @export var pathPoints : Array[Vector2]
 
 var i = 0
@@ -8,9 +10,8 @@ func _pointReached() -> bool:
 	return position.distance_to(pathPoints[i]) < 0.5
 	
 func _moveTowards() -> void:
-	pass
-	#if(_pointReached(pathPoints[i])):
-		#++i
-		#move(pathPoints[i])
-		#print("Moving towards position ", pathPoints[i])
+	if(_pointReached()):
+		++i
+		movement_component.move(pathPoints[i])
+		print("Moving towards position ", pathPoints[i])
 		
