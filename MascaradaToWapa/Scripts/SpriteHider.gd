706a5df7ec21649ffa4f6d4bf0d9006ab141697a
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-01-30 19:53:23 +0100

    Signal bus

---
+++ MascaradaToWapa/Scripts/SpriteHider.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+# Booleans to set with which masks will the object turn visible
+@export var default: bool
+@export var object: bool
+
+func _ready() -> void:
+	_turn_off()
+	if default:
+		SignalBus.connect("default_mask_on", _turn_on)
+	if object:
+		SignalBus.connect("object_mask_on", _turn_on)
+	SignalBus.connect("mask_off", _turn_off)
+
+func _turn_on() -> void:
+	var children = get_children()
+	for i in children:
+		i.visible = true
+
+func _turn_off() -> void:
+	var children = get_children()
+	for i in children:
+		i.visible = false
