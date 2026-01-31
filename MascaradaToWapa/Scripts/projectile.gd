b275ea78c36e8a67b451a133313792fb50892680
Javier Valseca <javivals@ucm.es>
2026-01-31 18:54:44 +0100

    projectile gets deltede on ojdfapsnkewajsd kg,fc

--- MascaradaToWapa/Scripts/projectile.gd
+++ MascaradaToWapa/Scripts/projectile.gd
@@ -2,6 +2,11 @@ extends RigidBody2D
 
 @export var speed := 5.0
 
+func _ready() -> void:
+	body_entered.connect(papearse)
+
+func papearse(_o):
+	queue_free()
+
 func shoot(dir :Vector2) -> void:
 	linear_velocity = dir * speed
-	
