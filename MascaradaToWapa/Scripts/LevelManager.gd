94495c5de873494dd41071efc262b38cd43da645
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-01-31 12:54:47 +0100

    level manager

---
+++ MascaradaToWapa/Scripts/LevelManager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var currentLevel: int = 0
+
+func getCurrentLevel() -> int:
+	return currentLevel
+
+func changeLevel(path: String) -> void:
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2})
+	currentLevel += 1
+
+func changeLevel_call(path: String, on_ready: Callable) -> void:
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2, "on_ready": on_ready})
+	currentLevel += 1
+
+func resetLevelToZero() -> void:
+	currentLevel = 0
+
+func resetLevelToZeroAndChangeScene(path: String) -> void:
+	resetLevelToZero()
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2})
