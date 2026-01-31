13e2e7578bbe262eb245971aacefefbd9ccea722
Alec Sánchez Gaitán <asanch42@ucm.es>
2026-01-31 15:32:24 +0100

    Door with key works
    -La llave abre la puerta cuando colisiona con ella

--- MascaradaToWapa/Scripts/key.gd
+++ MascaradaToWapa/Scripts/key.gd
@@ -1,7 +1,21 @@
-extends RigidBody2D
+extends draggable
 
 @export var doorObject : Node2D
+@export var area:Area2D
 
-func _on_collision(body: Node) -> void:
-	if(body is Node2D && body == doorObject): 
-		doorObject.open()
+func _ready() -> void:
+	area.area_entered.connect(_door_enters_area)
+	pass; # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass;
+
+
+func _door_enters_area(_area) -> void:
+	#El botón se pulsa, manda una señal para avisar de que se ha pulsado y se reproduce la animación
+	if(_area.get_parent() is Door):
+		_area.get_parent()._activate()
+	else:
+		pass
