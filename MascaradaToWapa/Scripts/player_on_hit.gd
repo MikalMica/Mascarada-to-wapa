4c9552703aabdb5bb2fbd8c8e28ee83d00732308
guillesc05 <guillesc05@gmail.com>
2026-01-31 08:30:01 -0500

    checpoint

--- MascaradaToWapa/Scripts/player_on_hit.gd
+++ MascaradaToWapa/Scripts/player_on_hit.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
-
+@export var player_node: Node2D
+var last_checkpoint_node: LastCheckpointPosition
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(_on_enemy_hit)
 	pass # Replace with function body.
 
 func _on_enemy_hit(other):
-	print("ay")
+	last_checkpoint_node = get_tree().get_first_node_in_group(&"checkpoint")
+	player_node.position = last_checkpoint_node.checkpoint_pos
+	# deberia reiniciar la posicion del jugador a la posicion del ultimo checkpoint
