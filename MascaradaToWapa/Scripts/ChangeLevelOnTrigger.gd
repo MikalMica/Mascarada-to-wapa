059708a24eb4bdb6da41f1aa2e871fad81df36f8
Javier Valseca <javivals@ucm.es>
2026-01-31 17:23:23 +0100

    arregladas animaciones del jugador

--- MascaradaToWapa/Scripts/ChangeLevelOnTrigger.gd
+++ MascaradaToWapa/Scripts/ChangeLevelOnTrigger.gd
@@ -4,5 +4,5 @@ extends Area2D
 
 # Only player should collide with this since is the only object with collision layer 8
 func _on_body_entered(body: Node2D) -> void:
-	var path = "res://Scenes/Scenarios/Level_" + str(levelID) + ".tscn"
+	var path = "res://Scenes/Levels/Level_" + str(levelID) + ".tscn"
 	LevelManager.changeLevel(path)
