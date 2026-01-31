ba6c90ca9554af4be7eaec1110d26d027fda9ae2
guillesc05 <guillesc05@gmail.com>
2026-01-31 07:37:37 -0500

    player hitbox

--- MascaradaToWapa/Scripts/player_on_hit.gd
+++ MascaradaToWapa/Scripts/player_on_hit.gd
@@ -1,11 +1,10 @@
-extends Node
+extends Area2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	area_entered.connect(_on_enemy_hit)
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_enemy_hit(other):
+	print("ay")
