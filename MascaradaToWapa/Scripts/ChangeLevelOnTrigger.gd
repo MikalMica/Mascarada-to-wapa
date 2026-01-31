8eb055a6bf366aeb532e8f1fe78e55dd2f0ce1b3
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-01-31 14:27:58 +0100

    level1 placeholder

--- MascaradaToWapa/Scripts/ChangeLevelOnTrigger.gd
+++ MascaradaToWapa/Scripts/ChangeLevelOnTrigger.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-@export var path: String
+@export var levelID: int
 
 # Only player should collide with this since is the only object with collision layer 8
 func _on_body_entered(body: Node2D) -> void:
+	var path = "res://Scenes/Scenarios/Level_" + str(levelID) + ".tscn"
 	LevelManager.changeLevel(path)
--- MascaradaToWapa/Scripts/LevelManager.gd
+++ MascaradaToWapa/Scripts/LevelManager.gd
@@ -6,11 +6,11 @@ func getCurrentLevel() -> int:
 	return currentLevel
 
 func changeLevel(path: String) -> void:
-	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2})
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2, "invert_on_leave": false})
 	currentLevel += 1
 
 func changeLevel_call(path: String, on_ready: Callable) -> void:
-	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2, "on_ready": on_ready})
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2, "on_ready": on_ready, "invert_on_leave": false})
 	currentLevel += 1
 
 func resetLevelToZero() -> void:
@@ -18,4 +18,4 @@ func resetLevelToZero() -> void:
 
 func resetLevelToZeroAndChangeScene(path: String) -> void:
 	resetLevelToZero()
-	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2})
+	SceneManager.change_scene(path, {"pattern": "curtains", "color": Color(0.614, 0.096, 0.177, 1.0), "wait_time": 2, "invert_on_leave": false})
