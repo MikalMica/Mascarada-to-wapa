554e18b5eeadc2e2a63107b5d6a6d6291f8a35e0
Javier Valseca <javivals@ucm.es>
2026-01-31 17:18:57 +0100

    movement component only uses velcity and not forces

--- MascaradaToWapa/Scripts/movement_component.gd
+++ MascaradaToWapa/Scripts/movement_component.gd
@@ -1,8 +1,7 @@
 extends RigidBody2D
 class_name Character
 
-@export var force := 2500.0
-@export var maxSpeed := 150.0
+@export var maxSpeed := 500.0
 
 var _dir = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +10,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	
-	apply_central_force(_dir * force)
-	if linear_velocity.length() > maxSpeed:
-		linear_velocity = _dir * maxSpeed
+	#apply_central_force(_dir * force)
+	#if linear_velocity.length() > maxSpeed:
+	linear_velocity = _dir * maxSpeed
 	
 
 func move(direction: Vector2) -> void:
